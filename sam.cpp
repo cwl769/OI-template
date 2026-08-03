9057e645b68f111340cd581646a011c8937722b3
cwl769 <cwl070609@outlook.com>
2026-08-03 21:31:12 +0800

    Feat: add SAM (suffix automaton)
    Implement a SAM using pointer mainly.
    Transfers are saved in a std::map, which will save
    memory but cost more time.

---
+++ sam.cpp
@@ -0,0 +1,72 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <cassert>
+typedef long long i64;
+typedef std::vector<int> veci;
+typedef std::vector<i64> veci64;
+typedef std::pair<int, int> PII;
+ 
+int tot = -1;
+struct SAM {
+    struct Node {
+        int id, len;
+        Node *link;
+        std::map<char, Node*> next;
+        Node() {
+            ++tot;
+            id = tot;
+            len = 0;
+            link = NULL;
+        }
+    };
+ 
+    std::vector<Node*> all_nodes;
+ 
+    Node *root, *last;
+    SAM() {
+        root = new Node;
+        all_nodes.emplace_back(root);
+        last = root;
+    }
+    Node *append(char c) {
+        Node *cur = new Node;
+        all_nodes.emplace_back(cur);
+        cur->len = last->len + 1;
+        Node *p = last;
+        for (; p != NULL; p = p->link) {
+            if (p->next.find(c) != p->next.end()) {
+                break;
+            } else {
+                p->next[c] = cur;
+            }
+        }
+        if (p == NULL) {
+            cur->link = root;
+            last = cur;
+            return cur;
+        }
+        Node *q = p->next[c];
+        if (p->len + 1 == q->len) {
+            cur->link = q;
+        } else {
+            Node *cq = new Node;
+            all_nodes.emplace_back(cq);
+            cq->len = p->len + 1;
+            cq->link = q->link;
+            cq->next = q->next;
+             
+            q->link = cq;
+            cur->link = cq;
+ 
+            for (; p != NULL && p->next.find(c) != p->next.end() && p->next[c] == q; p = p->link) {
+                p->next[c] = cq;
+            }
+        }
+        last = cur;
+        return cur;
+    }
+};
